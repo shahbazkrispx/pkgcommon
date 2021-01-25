3e7e115854e3de2e9060228b7a4a5217690e3c86
shahbazkrispx <shahbaz.khan@krispx.com>
2021-01-25 13:13:40 +0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shahbazkrispx/pkgcommon
+
+go 1.15
---
+++ response-builder.go
@@ -0,0 +1,18 @@
+package pkgcommon
+
+
+type Response struct {
+	Status bool `json:"status"`
+	Message string `json:"message"`
+	Data interface{} `json:"data,omitempty"`
+	Error interface{} `json:"error,omitempty"`
+}
+
+func ResponseBuilder(status bool, msg string, data interface{}, errors interface{}) Response {
+	return Response{
+		Status: status,
+		Message: msg,
+		Data: data,
+		Error: errors,
+	}
+}
