38fdf94d1806b9cabc062faaf80a339314689f6c
shahbazkrispx <shahbaz.khan@krispx.com>
2021-05-27 16:47:22 +0500

    inventory constants update

--- contants.go
+++ contants.go
@@ -2,23 +2,38 @@ package pkgcommon
 
 const (
 	//Inventory Constants
+	deActive        = 0
 	active          = 1
 	soldOut         = 2
+	pending         = 3
+	carDeleted      = "CarDeleted"
+	carSoldOut      = "CarSoldOut"
+	carUpdated      = "CarUpdated"
 	inventoryDomain = "CarMarketPlace"
 )
 
 //inventoryConstants
 type inventoryConstants struct {
-	Domain  string
-	Active  uint8
-	SoldOut uint8
+	Domain     string
+	DeActive   uint8
+	Active     uint8
+	SoldOut    uint8
+	Pending    uint8
+	CarSoldOut string
+	CarUpdated string
+	CarDeleted string
 }
 
 //GetInventoryConstants return inventoryConstants
 func GetInventoryConstants() inventoryConstants {
 	return inventoryConstants{
-		Domain:  inventoryDomain,
-		Active:  active,
-		SoldOut: soldOut,
+		Domain:     inventoryDomain,
+		DeActive:   deActive,
+		Active:     active,
+		SoldOut:    soldOut,
+		Pending:    pending,
+		CarDeleted: carDeleted,
+		CarSoldOut: carSoldOut,
+		CarUpdated: carUpdated,
 	}
 }
