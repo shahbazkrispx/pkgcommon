d2c9b72f08098d9d629807eb287a0ea26ccb743a
shahbazkrispx <shahbaz.khan@krispx.com>
2021-08-02 14:52:35 +0500

    add in memoery cache

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-redis/redis/v7 v7.4.0
 	github.com/joho/godotenv v1.3.0
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 )
---
+++ in-mem-cache.go
@@ -0,0 +1,56 @@
+package pkgcommon
+
+import (
+	"fmt"
+	"github.com/patrickmn/go-cache"
+	"os"
+	"strconv"
+	"time"
+)
+
+type InMemPostCache interface {
+	Set(key string, value interface{}, exp time.Duration) error
+	Get(key string) (string, error)
+	Delete(Key string) (int64, error)
+}
+
+type inMemCache struct {
+	DefaultExpiration time.Duration
+	CleanUpInterval   time.Duration
+}
+
+func NewMemCache() InMemPostCache {
+	expVal, _ := strconv.ParseInt(os.Getenv("IN_MEM_DEFAULT_EXP"), 10, 64)
+	cleanUp, _ := strconv.ParseInt(os.Getenv("IN_MEM_CLEANUP_INTERVAL"), 10, 64)
+
+	return &inMemCache{
+		DefaultExpiration: time.Duration(int(expVal)) * time.Minute,
+		CleanUpInterval:   time.Duration(int(cleanUp)) * time.Minute,
+	}
+}
+
+func (inMem *inMemCache) getInMemClient() *cache.Cache {
+	return cache.New(inMem.DefaultExpiration, inMem.CleanUpInterval)
+}
+
+func (inMem *inMemCache) Set(key string, value interface{}, exp time.Duration) error {
+	c := inMem.getInMemClient()
+	c.Set(key, value, exp)
+	return nil
+}
+
+func (inMem *inMemCache) Get(key string) (string, error) {
+	c := inMem.getInMemClient()
+	val, found := c.Get(key)
+	if !found {
+		return "", fmt.Errorf("%v Not Found", key)
+	}
+
+	return val.(string), nil
+}
+
+func (inMem *inMemCache) Delete(key string) (int64, error) {
+	c := inMem.getInMemClient()
+	c.Delete(key)
+	return 0, nil
+}
