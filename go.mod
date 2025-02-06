2f3fa9fbcbb8e07707c1ec688b9791f2ab44999a
Shahbaz <shahbaz.khan@krispx.com>
2025-02-06 16:31:44 +0500

    sqs subscriber code added

--- go.mod
+++ go.mod
@@ -4,22 +4,36 @@ go 1.22
 
 require (
 	github.com/aws/aws-sdk-go v1.55.5
+	github.com/aws/aws-sdk-go-v2/config v1.29.6
+	github.com/aws/aws-sdk-go-v2/service/sqs v1.37.14
 	github.com/go-redis/redis/v7 v7.4.1
+	github.com/gofrs/uuid/v5 v5.3.0
 	github.com/joho/godotenv v1.5.1
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	gorm.io/datatypes v1.2.5
+	gorm.io/gorm v1.25.11
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.36.1 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.59 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.28 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.32 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.32 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.13 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.24.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.28.14 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.33.14 // indirect
+	github.com/aws/smithy-go v1.22.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
-	github.com/gofrs/uuid/v5 v5.3.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gorm.io/driver/mysql v1.5.6 // indirect
-	gorm.io/gorm v1.25.11 // indirect
 )
---
+++ sqs-subcriber.go
@@ -0,0 +1,148 @@
+package pkgcommon
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/aws/aws-sdk-go/aws"
+)
+
+const (
+	maxWorkers        = 100
+	maxMessages       = 10
+	waitTimeSeconds   = 20
+	visibilityTimeout = 30
+)
+
+type SQSSubscriber struct {
+	client      *sqs.Client
+	queueURL    *string
+	workerCount int
+	handlers    []MessageHandler
+	wg          sync.WaitGroup
+	ctx         context.Context
+	cancel      context.CancelFunc
+}
+
+type MessageHandler func(msg *types.Message) error
+
+func NewSQSSubscriber(queueName string, workerCount int) (*SQSSubscriber, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+
+	client := sqs.NewFromConfig(cfg)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	if os.Getenv("APP_ENV") != "prod" && os.Getenv("APP_ENV") != "production" {
+		queueName = fmt.Sprintf("%s_%s", os.Getenv("APP_ENV"), queueName)
+	}
+
+	result, err := client.GetQueueUrl(ctx, &sqs.GetQueueUrlInput{
+		QueueOwnerAWSAccountId: aws.String(os.Getenv("AWS_ACCOUNT_ID")),
+		QueueName:              aws.String(queueName),
+	})
+	if err != nil {
+		cancel()
+		return nil, err
+	}
+
+	return &SQSSubscriber{
+		client:      client,
+		queueURL:    result.QueueUrl,
+		workerCount: workerCount,
+		ctx:         ctx,
+		cancel:      cancel,
+	}, nil
+}
+
+func (s *SQSSubscriber) AddHandler(handler MessageHandler) {
+	s.handlers = append(s.handlers, handler)
+}
+
+func (s *SQSSubscriber) Start() {
+	for i := 0; i < s.workerCount; i++ {
+		s.wg.Add(1)
+		go s.startWorker()
+	}
+}
+
+func (s *SQSSubscriber) Stop() {
+	s.cancel()
+	s.wg.Wait()
+}
+
+func (s *SQSSubscriber) startWorker() {
+	defer s.wg.Done()
+
+	for {
+		select {
+		case <-s.ctx.Done():
+			return
+		default:
+			messages, err := s.receiveMessages()
+			if err != nil {
+				log.Printf("Error receiving messages: %v", err)
+				time.Sleep(time.Second)
+				continue
+			}
+
+			for _, msg := range messages.Messages {
+				s.processMessage(&msg)
+			}
+		}
+	}
+}
+
+func (s *SQSSubscriber) receiveMessages() (*sqs.ReceiveMessageOutput, error) {
+	output, err := s.client.ReceiveMessage(context.TODO(), &sqs.ReceiveMessageInput{
+		AttributeNames: []types.QueueAttributeName{
+			types.QueueAttributeNameAll,
+		},
+		MessageAttributeNames: []string{
+			"All",
+		},
+		QueueUrl:            s.queueURL,
+		MaxNumberOfMessages: maxMessages,
+		WaitTimeSeconds:     waitTimeSeconds,
+		VisibilityTimeout:   visibilityTimeout,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
+
+func (s *SQSSubscriber) processMessage(msg *types.Message) {
+	var processError error
+
+	// Execute all handlers for the message
+	for _, handler := range s.handlers {
+		if err := handler(msg); err != nil {
+			processError = err
+			log.Printf("Error processing message: %v", err)
+			break
+		}
+	}
+
+	// If message was processed successfully, delete it
+	if processError == nil {
+		_, err := s.client.DeleteMessage(context.TODO(), &sqs.DeleteMessageInput{
+			QueueUrl:      s.queueURL,
+			ReceiptHandle: msg.ReceiptHandle,
+		})
+		if err != nil {
+			log.Printf("Error deleting message: %v", err)
+		}
+	}
+}
